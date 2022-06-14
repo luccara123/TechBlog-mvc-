b04a339c3e8dea464b402a7baef6aa5497d223cf
Lucca Araujo <lucca.ara7@gmail.com>
2022-06-13 23:20:37 -0700

    added home and dashboard routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Get all
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: ['id', 'post_title', 'post_content', 'created_at'],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_content', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(response => res.json(response))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// Get by ID
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id','post_title','post_content','created_at'],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_content', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(response => {
+            if (!response) {
+                res.status(404).json({
+                    message: 'No user in the database with this id.'
+                });
+                return;
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+// Get all
+router.get('/', (req, res) => {
+    Post.findAll({
+            attributes: [ 'id', 'post_title', 'post_content', 'created_at' ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_content', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(response => res.json(response))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//Get by ID
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id','post_title', 'post_content','created_at'],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_content', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(response => {
+            if (!response) {
+                res.status(404).json({
+                    message: ''
+                });
+                return;
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
