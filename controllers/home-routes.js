ab148bdcdbcafce6a3cf67fa77f8a83263c0916d
Lucca Araujo <lucca.ara7@gmail.com>
2022-06-23 22:58:37 -0700

    fixed problem with heroku

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
-const withAuth = require('../../utils/auth');
+const withAuth = require('../utils/auth');
 
 // Get all
 router.get('/', (req, res) => {
